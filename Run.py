347d345af88ec332a1d8979ee08b8edae27abc6a
Rozhak <65714340+RozhakXD@users.noreply.github.com>
2023-12-12 19:58:53 +0700

    Add files via upload

--- Run.py
+++ Run.py
@@ -4,16 +4,20 @@ def Subprocess_Call(command):
     subprocess.call(command)
 
 if __name__ == '__main__':
-    if platform.machine() == "aarch64":
-        command = ["chmod", "+x", "aarch64"]
-        Subprocess_Call(command)
-        command = ["./aarch64"]
-        Subprocess_Call(command)
-    elif platform.machine() == "x86_64":
-        command = ["chmod", "+x", "x86_64"]
-        Subprocess_Call(command)
-        command = ["./x86_64"]
-        Subprocess_Call(command)
-    else:
-        print(f"Perangkat {platform.machine()} bit, tidak dapat menjalankan aarch64 atau x86_64!")
+    try:
+        if platform.machine() == "aarch64":
+            command = ["chmod", "+x", "aarch64"]
+            Subprocess_Call(command)
+            command = ["./aarch64"]
+            Subprocess_Call(command)
+        elif platform.machine() == "x86_64":
+            command = ["chmod", "+x", "x86_64"]
+            Subprocess_Call(command)
+            command = ["./x86_64"]
+            Subprocess_Call(command)
+        else:
+            print(f"Perangkat {platform.machine()} bit, tidak dapat menjalankan aarch64 atau x86_64!")
+            exit()
+    except (Exception, KeyboardInterrupt):
+        print(f"[Error] {str(e).capitalize()}!")
         exit()
